ac841af0d6e87062b89729b0676ebdaa08853287
Simon-157 <boatengsimonjnr157@gmail.com>
2023-12-12 11:45:52 +0000

    feat: local auth with passport added

---
+++ src/auth/localAuth.ts
@@ -0,0 +1,90 @@
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt"
+import { pool } from "../config/psql";
+import { logger } from "../config/logger";
+import passport from "passport";
+import { UserDTO } from "../types/User";
+
+
+const parseToUserDTO = (params: Record<any, any>): UserDTO => {
+  const parsed = {
+    userId: params.user_id,
+    email: params.email,
+    userName: params.user_name,
+    avatarUrl: params.avatar_url,
+    displayName: params.display_name,
+    role:params.role,
+    bio: params.bio,
+    currentRoomId: params.current_room_id,
+    lastSeen: params.last_seen,
+    createdAt: params.created_at,
+  };
+
+  return parsed;
+};
+
+
+
+
+const localStrategyMiddleware = new LocalStrategy(
+  {
+    usernameField: "email",
+    passwordField: "password",
+  },
+  async (email: any, password: any, done: any) => {
+    try {
+      const { rows } = await pool.query(
+        `
+        SELECT * FROM user_data WHERE email = $1;
+        `,
+        [email]
+      );
+
+      if (rows.length === 0) {
+        return done(null, false, { message: "Incorrect email." });
+      }
+
+      const user = rows[0];
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+
+      const parsedUser = parseToUserDTO(user);
+      return done(null, parsedUser);
+    } catch (error) {
+      return done(error);
+    }
+  }
+);
+
+
+
+const serializeMiddleware = (user: Partial<UserDTO>, done: any) => {
+  logger.info(user)
+  done(null, user.userId);
+};
+
+const deserializeMiddleware = async (userId: string, done: any) => {
+  try {
+    const { rows } = await pool.query(
+      `
+      SELECT *
+      FROM user_data u
+      WHERE u.user_id = $1;
+      `,
+      [userId]
+    );
+    const parsedUserData = parseToUserDTO(rows[0]);
+    logger.info(parsedUserData)
+    done(null, parsedUserData);
+  } catch (err) {
+    logger.log({ level: "error", message: `${err}` });
+    done(err, null);
+  }
+};
+
+passport.use(localStrategyMiddleware);
+passport.serializeUser(serializeMiddleware);
+passport.deserializeUser(deserializeMiddleware);
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,12 @@
+import  bcrypt from 'bcrypt';
 import "dotenv/config";
 import { NextFunction, Request, Response, Router } from "express";
 import passport from "passport";
 import "../auth/googleAuth";
 import "../auth/microsoftAuth";
+import "../auth/localAuth";
 import createHttpError from "http-errors";
+import { pool } from "../config/psql";
 
 export const router = Router();
 
@@ -49,6 +52,56 @@ router.post("/logout", (req: Request, res: Response, next: NextFunction) => {
         ? "Currently authenicated"
         : " Currently unauthenticated",
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+
+
+router.post(
+  "/local/login",
+  
+  passport.authenticate("local", {
+    successRedirect:
+      process.env.NODE_ENV == "production"
+        ? process.env.CLIENT_URI_CALLBACK_PROD
+        : process.env.CLIENT_URI_CALLBACK,
+    failureRedirect: "/failure",
+    failureFlash: true,
+  })
+);
+
+router.post("/local/signup", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const query = {
+      text: 'SELECT * FROM user_data WHERE email = $1',
+      values: [email],
+    };
+
+    const result = await pool.query(query);
+
+    if (result.rows.length > 0) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const insertQuery = {
+      text: 'INSERT INTO user_data (email, password) VALUES ($1, $2) RETURNING *',
+      values: [email, hashedPassword],
+    };
+
+    const newUser = await pool.query(insertQuery);
+    const url = process.env.NODE_ENV == "production"
+        ? process.env.CLIENT_URI_CALLBACK_PROD
+        : process.env.CLIENT_URI_CALLBACK;
+
+    res.redirect(url!);
+
   } catch (error) {
     next(error);
   }
