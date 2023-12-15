efc1ad91d558a4d2fc988fd55577e2f42d162b41
Simon-157 <boatengsimonjnr157@gmail.com>
2023-12-15 08:37:37 +0000

    feat: swearwords api changed

--- src/routes/swearWordsRoutes.ts
+++ src/routes/swearWordsRoutes.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 const swearRouter = express.Router();
 swearRouter.get('/check', async (req:Request, res:Response) => {
   const { text } = req.query;
-  const API_KEY ='cf49873a30f57f4881fc7f9f102582d4';
+  const API_KEY = '72f24e0422847e10e93abce9591fb3ac'
   const endpoint = `http://api1.webpurify.com/services/rest/?method=webpurify.live.check&api_key=${API_KEY}&text=${encodeURIComponent(text as string)}&format=json`;
   try {
     const response = await axios.get(endpoint);
